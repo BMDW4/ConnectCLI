8ba49df72e6774fd5c4c556236b4724d76a7cb73
OscoreJohnSteiner <83648327+OscoreJohnSteiner@users.noreply.github.com>
2021-10-26 15:52:52 +0100

    added error base and edited wmdaconnectexception to use errorbase for middleware

--- WmdaConnect.Shared/WmdaConnectException.cs
+++ WmdaConnect.Shared/WmdaConnectException.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 namespace WmdaConnect.Shared
 {
     public class WmdaConnectException : Exception
     {
+        public List<ErrorBase> Errors { get; } = new List<ErrorBase>();
         public WmdaConnectException(string message) : base(message)
         {
         }
