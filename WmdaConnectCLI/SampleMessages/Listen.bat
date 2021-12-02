35cb9cc6d365602ce8dc0dca5ed8fcb439d71d6c
John Gibbons <jg11jg@outlook.com>
2021-12-02 10:52:24 +0000

    removed clientSecrets even outdated ones

--- WmdaConnectCLI/SampleMessages/Listen.bat
+++ WmdaConnectCLI/SampleMessages/Listen.bat
@@ -2,7 +2,11 @@
 cls
 @ECHO [1. Remember to update the env, clientId and clientSecret]
 @ECHO [2. that recipient registry (i.e. the listener) and the registry doing the sending correspond in the sample messages json]
-REM Status: { "DisplayName": "", "ClientId": "bdec1779-8a0a-4f86-a85d-b52d14de9403", "ClientSecret": "d283fa9d-ba55-42ef-9ced-5882b31686e0", "QueueName": "9999" }
-wm listen --clientId bdec1779-8a0a-4f86-a85d-b52d14de9403 --ClientSecret d283fa9d-ba55-42ef-9ced-5882b31686e0 --env stn7
+SET clientId=<clientId (Guid)>
+SET clientSecret=<clientSecret (Guid or complex text)>
+SET env_number=<env_number (integer value)>
+
+wm listen --clientId %clientId% --clientSecret %clientSecret% --env %env_number%
+
 @popd
 @pause
