0942d43ed7e7a1894f339044295bb12747d29ca9
John Gibbons <jg11jg@outlook.com>
2022-01-05 08:15:44 +0000

    going back to old namespacing style

--- WmdaConnect.Models/IdTypes/IHasDonorAltStatus.cs
+++ WmdaConnect.Models/IdTypes/IHasDonorAltStatus.cs
@@ -1,15 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
-namespace WmdaConnect.Models.IdTypes;
+namespace WmdaConnect.Models.IdTypes
+{
 
 //Attributes here have no effect on swagger, for reference only and ease of copy/paste
-public interface IHasDonorAltStatus
-{
-    /// <summary>
-    /// Donor ALT status D_ALT Opt 3
-    /// </summary>
-    [Range(0,999)]
-    [JsonProperty("alt")]
-    public int? DonorAltStatus { get; set; }
+    public interface IHasDonorAltStatus
+    {
+        /// <summary>
+        /// Donor ALT status D_ALT Opt 3
+        /// </summary>
+        [Range(0, 999)]
+        [JsonProperty("alt")]
+        public int? DonorAltStatus { get; set; }
+    }
 }
