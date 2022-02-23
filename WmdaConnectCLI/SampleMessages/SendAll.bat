09b233e522c253a12db555ca5440d458fe79ae38
John Gibbons <john@oscore.io>
2022-02-23 15:11:15 +0000

    made pauses in SendAll configurable

--- WmdaConnectCLI/SampleMessages/SendAll.bat
+++ WmdaConnectCLI/SampleMessages/SendAll.bat
@@ -7,43 +7,44 @@ SET clientId=<clientId (Guid)>
 SET clientSecret=<clientSecret (Guid or complex text)>
 SET environment=<environment number (integer value)>
 SET targetRegistry=<registry ion to send to e.g. 1234>
+SET pause=0
 
 wm connect --clientId %clientId% --clientSecret %clientSecret% --environment %environment%
-@pause
+@IF %pause%==1 pause
 wm message --messageType TextMessage --file "./SampleMessages/TextMessage.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType TypingRequest --file "./SampleMessages/TypingRequest.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType TypingResponse --file "./SampleMessages/TypingResponse.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType SampleRequest --file "./SampleMessages/SampleRequest.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType SampleArrival --file "./SampleMessages/SampleArrival.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType SampleResponse --file "./SampleMessages/SampleResponse.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType CordBloodUnitReportRequest --file "./SampleMessages/CordBloodUnitReportRequest.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType InfectiousDiseaseMarkerRequest --file "./SampleMessages/InfectiousDiseaseMarkerRequest.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType InfectiousDiseaseMarkerResult --file "./SampleMessages/InfectiousDiseaseMarkerResult.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType MessageAcknowledgement --file "./SampleMessages/MessageAcknowledgement.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType MessageDenial --file "./SampleMessages/MessageDenial.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType NoResult --file "./SampleMessages/NoResult.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType RequestCancellation --file "./SampleMessages/RequestCancellation.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType ReservationRequest --file "./SampleMessages/ReservationRequest.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType ReservationResult --file "./SampleMessages/ReservationResult.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType ResultReminder --file "./SampleMessages/ResultReminder.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType SampleInfo --file "./SampleMessages/SampleInfo.json" --targetRegistry %targetRegistry%
-@pause
+@IF %pause%==1 pause
 wm message --messageType Warning --file "./SampleMessages/Warning.json" --targetRegistry %targetRegistry%
 @popd
 @pause
